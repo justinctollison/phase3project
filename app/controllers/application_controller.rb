b24b8d46928264b8a04e9450e9da60fa54a44d4e
justinctollison <justinctollison@gmail.com>
2022-04-04 17:01:15 -0700

    Completed migrations, models, seed data, controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,41 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/pokemon" do
+    pokemon = Pokemon.all
+    pokemon.to_json(only: [:name, :location, :move], include: {
+      reviews: {only: [:rating, :text]}
+    })
+  end
+
+  get "/pokemon/:id" do
+    pokemon = Pokemon.find(params[:id])
+    pokemon.to_json(only: [:name, :location, :move], include: {
+      reviews: {only: [:rating, :text]}
+    })
+  end
+
+  post "/reviews" do
+    review = Review.create(
+      rating: params[:rating],
+      text: params[:text]
+    )
+    review.to_json
+  end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(
+      rating: params[:rating],
+      text: params[:text]
+    )
+    review.to_json
+  end
+
 end
